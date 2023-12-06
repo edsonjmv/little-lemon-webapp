955ed9140c6f51a0ea1101d59f42851faa875a0c
MACHE022 <edson.machado.-nd@disney.com>
2023-12-06 11:01:18 +0100

    Remove bootstrap

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function Nav() {
   return (
-    <nav className="p-3 d-flex justify-content-between align-content-center">
+    <nav>
       <img src="/logo.svg" />
-      <ul className="d-flex align-items-center mb-0 list-unstyled gap-4 text-uppercase">
+      <ul>
         <li>
           <a>Home</a>
         </li>
