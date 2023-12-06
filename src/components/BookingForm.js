65945d4c8eccf89a0e491eaeee435c97100ca1be
MACHE022 <edson.machado.-nd@disney.com>
2023-12-06 21:00:48 +0100

    Defining the booking page

---
+++ src/components/BookingForm.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+
+export default function BookingForm({ form, onFieldChange, onSubmit }) {
+  const [availableTimes] = useState([
+    '17:00',
+    '18:00',
+    '19:00',
+    '20:00',
+    '21:00',
+    '22:00',
+    '23:00'
+  ]);
+  return (
+    <form onSubmit={onSubmit}>
+      <label htmlFor="res-date">Choose date</label>
+      <input
+        type="date"
+        id="res-date"
+        name="date"
+        onChange={onFieldChange}
+        value={form.date}
+      />
+      <label htmlFor="res-time">Choose time</label>
+      <select
+        id="res-time"
+        name="time"
+        onChange={onFieldChange}
+        value={form.time}
+      >
+        {availableTimes.map((time) => (
+          <option key={time} value={time}>
+            {time}
+          </option>
+        ))}
+      </select>
+      <label htmlFor="guests">Number of guests</label>
+      <input
+        type="number"
+        placeholder="1"
+        min="1"
+        max="10"
+        id="guests"
+        name="guests"
+        onChange={onFieldChange}
+        value={form.guests}
+      />
+      <label htmlFor="occasion">Occasion</label>
+      <select
+        id="occasion"
+        name="occasion"
+        onChange={onFieldChange}
+        value={form.occasion}
+      >
+        <option value="birthday">Birthday</option>
+        <option value="anniversary">Anniversary</option>
+      </select>
+      <input type="submit" value="Make Your reservation" />
+    </form>
+  );
+}
--- src/components/BookingPage.js
+++ src/components/BookingPage.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Layout from './Layout';
+import BookingForm from './BookingForm';
 
 export default function BookingPage() {
+  const [form, setForm] = useState({
+    date: '',
+    time: '20:00',
+    guests: '1',
+    occasion: 'birthday'
+  });
+
+  function handleFieldChange(e) {
+    const { name, value } = e.target;
+
+    setForm((prev) => ({
+      ...prev,
+      [name]: value
+    }));
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(form);
+  }
+
   return (
     <Layout>
-      <main>BookingPage</main>
+      <main>
+        <BookingForm
+          form={form}
+          onFieldChange={handleFieldChange}
+          onSubmit={handleSubmit}
+        />
+      </main>
     </Layout>
   );
 }
